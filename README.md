2c0f60147a1d4457096d4d5cf79c1fed44a58bee
Robert J. Paruch <roobbb@live.com>
2019-06-20 01:51:15 -0700

    Updated the portfolio

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # robjpar.github.io
 
 ## Description
-A personal website created on GitHub Pages.
+
+A portfolio website created on GitHub Pages.
 
 ## How to use
-Access the deployed website though this link [robjpar](https://robjpar.github.io).
+
+Access the deployed website through this link [robjpar](https://robjpar.github.io).
--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,19 @@
 // Smooth scrolling using jQuery easing
 $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+    location.hostname == this.hostname
+  ) {
     var target = $(this.hash);
     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
     if (target.length) {
-      $('html, body').animate({
-        scrollTop: (target.offset().top - 56)
-      }, 1000, "easeInOutExpo");
+      $('html, body').animate(
+        {
+          scrollTop: target.offset().top - 56
+        },
+        1000,
+        'easeInOutExpo'
+      );
       return false;
     }
   }
